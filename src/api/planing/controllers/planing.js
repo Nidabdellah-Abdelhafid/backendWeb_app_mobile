0f9569d06a2e464759e78f88e19d8af2b13dd566
Nidabdellah-Abdelhafid <nidabdellahabdelhafid99@gmail.com>
2024-05-15 12:35:06 +0100

    first c

---
+++ src/api/planing/controllers/planing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * planing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::planing.planing');
---
+++ src/api/planing/routes/planing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * planing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::planing.planing');
